b0dd3524a9471895c777c6bfa2ed7bc191b14659
KinderRiven <1339764596@qq.com>
2021-06-22 16:41:59 +0800

    hsk:scheme

--- src/scheme.cc
+++ src/scheme.cc
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-06-22 16:18:12
- * @LastEditTime: 2021-06-22 16:20:30
+ * @LastEditTime: 2021-06-22 16:40:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /PIE/src/scheme.cc
  */
 
 #include "scheme.hpp"
-#include "single_scheme.hpp"
+#include "scheme/single/single_scheme.hpp"
 
 using namespace PIE;
 
