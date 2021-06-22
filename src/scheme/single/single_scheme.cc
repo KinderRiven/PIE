7169c26dbddbccccf309c7dd771de70e3ad6bb56
KinderRiven <1339764596@qq.com>
2021-06-22 16:44:45 +0800

    hsk:scheme

--- src/scheme/single/single_scheme.cc
+++ src/scheme/single/single_scheme.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-22 16:07:03
- * @LastEditTime: 2021-06-22 16:11:07
+ * @LastEditTime: 2021-06-22 16:44:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /PIE/src/scheme/single/single_scheme.cpp
@@ -23,23 +23,23 @@ Status SingleScheme::Insert(Slice& key, void* value)
 {
 }
 
-Status SingleScheme::Search(Slice key, void** value)
+Status SingleScheme::Search(Slice& key, void** value)
 {
 }
 
-Status SingleScheme::Update(Slice key, void* value)
+Status SingleScheme::Update(Slice& key, void* value)
 {
 }
 
-Status SingleScheme::Upsert(Slice key, void* value)
+Status SingleScheme::Upsert(Slice& key, void* value)
 {
 }
 
-Status SingleScheme::ScanCount(Slice startkey, size_t count, void** vec)
+Status SingleScheme::ScanCount(Slice& startkey, size_t count, void** vec)
 {
 }
 
-Status SingleScheme::Scan(Slice startkey, Slice endkey, void** vec)
+Status SingleScheme::Scan(Slice& startkey, Slice& endkey, void** vec)
 {
 }
 
