7c84616fcf54b5a78f3cd776ac124aacf68bd454
KinderRiven <1339764596@qq.com>
2021-06-22 16:44:01 +0800

    hsk:single_scheme

--- src/scheme/single/single_scheme.hpp
+++ src/scheme/single/single_scheme.hpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-22 15:23:31
- * @LastEditTime: 2021-06-22 16:19:33
+ * @LastEditTime: 2021-06-22 16:43:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /PIE/src/include/single_scheme.hpp
@@ -29,28 +29,28 @@ public:
     // Search and return related value of given key
     // If the key does not exist in current index, kNotFound would
     // be returned, otherwise kOk is returned
-    Status Search(Slice key, void** value);
+    Status Search(Slice& key, void** value);
 
     // Update specified key with given value.
     // If the key does not exist in current index, kNotFound would
     // be returned, otherwise return kOk if update success
-    Status Update(Slice key, void* value);
+    Status Update(Slice& key, void* value);
 
     // Update specified key with given value if target key exists
     // otherwise insert target key-value pair.
     // This interface always return kOk unless memory allocation error
     // occurs
-    Status Upsert(Slice key, void* value);
+    Status Upsert(Slice& key, void* value);
 
     // Scan from start key and return its "count" successors
     // Coresponding values are placed in a void* array specified by "vec"
     // However, values are not guaranteed to be SORTED;
-    Status ScanCount(Slice startkey, size_t count, void** vec);
+    Status ScanCount(Slice& startkey, size_t count, void** vec);
 
     // Scan to fetch keys within the range [startkey, endkey);
     // Coresponding values are placed in a void* array specified by "vec"
     // However, values are not guaranteed to be SORTED;
-    Status Scan(Slice startkey, Slice endkey, void** vec);
+    Status Scan(Slice& startkey, Slice& endkey, void** vec);
 
     // Printout Any related index message:
     // such as the height of B+Tree or max height of radix tree
