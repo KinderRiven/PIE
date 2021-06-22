9de69a68b1fb8bf4f5846a41455246dc7850c40c
KinderRiven <1339764596@qq.com>
2021-06-22 19:52:18 +0800

    hsk:shceme

--- src/scheme.cc
+++ src/scheme.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-22 16:18:12
- * @LastEditTime: 2021-06-22 16:47:35
+ * @LastEditTime: 2021-06-22 19:52:08
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /PIE/src/scheme.cc
@@ -16,4 +16,8 @@ Status Scheme::Create(const Options& options, Scheme** schemeptr)
 {
     *schemeptr = new SingleScheme(options);
     return Status::OK();
+}
+
+Scheme::~Scheme()
+{
 }
