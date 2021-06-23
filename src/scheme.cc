8d87f98101da8d00ea7290dd642431e75932f6be
KinderRiven <1339764596@qq.com>
2021-06-23 15:03:35 +0800

    hsk:single scheme

--- src/scheme.cc
+++ src/scheme.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-22 16:18:12
- * @LastEditTime: 2021-06-23 13:55:06
+ * @LastEditTime: 2021-06-23 15:03:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /PIE/src/scheme.cc
@@ -18,7 +18,7 @@ Status Scheme::Create(const Options& options, Scheme** schemeptr)
         *schemeptr = new SingleScheme(options);
         return Status::OK();
     } else {
-        return Status::NotSupported();
+        return Status::NotSupported("Create Scheme Failed.");
     }
 }
 
